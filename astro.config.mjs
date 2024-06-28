0da6f797f545d4c5a125b72760e2ad4779a85061
haloucp <3672332946@qq.com>
2024-06-28 20:53:59 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://haloucp.github.io",
+  site: "https://huoblog.chat",
   base: "myBlog",
 });
